c1823219211c52a875062838121c51e80f4baa46
Iwona007 <iwonka.adamkiewicz@gmail.com>
2020-06-21 22:56:26 +0200

    initial commit

---
+++ src/main/java/pl/iwona/securityencoderweek4/SecurityEncoderWeek4Application.java
@@ -0,0 +1,13 @@
+package pl.iwona.securityencoderweek4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityEncoderWeek4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityEncoderWeek4Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/iwona/securityencoderweek4/SecurityEncoderWeek4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.iwona.securityencoderweek4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityEncoderWeek4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
